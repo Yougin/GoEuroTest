d65bcbab28e9521560eca6745ae5f4fa82ae7787
Eugene Beletskiy <ebeletskiy@gmail.com>
2015-04-07 16:30:08 +0200

    Javadoc for WebService#getDestinationPoints() endpoint added.

--- src/main/java/ebeletskiy/goeuro/test/data/api/WebService.java
+++ src/main/java/ebeletskiy/goeuro/test/data/api/WebService.java
@@ -7,6 +7,12 @@ import retrofit.http.Path;
 
 public interface WebService {
 
+  /**
+   * The endpoint to fetch a list of destination points objects.
+   *
+   * @param city - you would like to get destination points for (from?)
+   * @return If no matches are found an empty JSON array is returned.
+   */
   @GET("/GoEuroAPI/rest/ebeletskiy.goeuro.test.data.api/v2/position/suggest/en/{city}")
   List<DestinationPoint> getDestinationPoints(@Path("city") String city);
 }
